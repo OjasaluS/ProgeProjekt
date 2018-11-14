05c7d30d8d33da1247260724a29f6c0b46c0c45c
unknown <siim.ojasalu@gmail.com>
2018-11-14 22:55:43 +0200

    lisa

--- projekt/funktsioon.py
+++ projekt/funktsioon.py
@@ -8,7 +8,11 @@ def algus():
 
 def es_r_1(sisend):
     if sisend == 1:
-        return print("you dead")
+        print("Tikud on läbi vettinud toas tunda olevast tugevast niiskusest. Ei lähe põlema.")
+        print("Mida soovid edasi teha?")
+        print("1.) Püüan aegamisi aknani jõuda ja sealt välja vaadata.")
+        print("2.) Koban mööda seinu, äkki leidub kuskil lüliti?")
+        valik_es_r_1 = int(input("Valik: "))
     if sisend == 2:
         print("Kuna toas on veel hämar siis välja vaadates näed justkui suurt seina, millele on tõmmatud okastraat. Kas su silmad petavad sind või viibid sa mingisuguses vanglataolises hoones? Jätad selle meelde")
         print("Mida soovid edasi teha?")
@@ -22,4 +26,21 @@ def es_r_1(sisend):
         valik_es_r_1 = int(input("Valik: "))
     return valik_es_r_1
 
-#def esimene_r_2(sisend):
+def es_r_2(algne, jätk):
+    if algne == 1:
+        if jätk == 1:
+            print("Kuna toas on veel hämar siis välja vaadates näed justkui suurt seina, millele on tõmmatud okastraat. Kas su silmad petavad sind või viibid sa mingisuguses vanglataolises hoones? Jätad selle meelde")
+            print("Enne, kui jõuad edasi tegutseda, lendab ruumi teises otsas pauguga lahti metalluks, mida sa pimedas märganud ei olnud. Ere valgus, mis ukseavast sisse kumab pimestab algselt, kuid kohe ilmub sinna ette jõulise mehe siluett, kelle nägu sa veel ei suuda tuvastada.")
+            print('"Mulle öeldi, et sa oled ärkvel. Meeldiv," lausub mees madalal häälel.')
+            print('"Kas sa tead, kus sa oled?"')
+            print("Vastan: ")
+            print('1.) "Pakun, et vanglas," jääb su vastus lühikeseks, kuid konkreetseks')
+            print('2.) "Mul pole õrna aimugi. Palun, laske mind vabaks. Mul pole midagi väärtuslikku!" halad viimses hädas.')
+            valik_es_r_2 = int(input("Valik: "))
+            return valik_es_r_2
+    if algne == 2:
+        if jätk == 1:
+            print("algne 2, jätk 1")
+    if algne == 3:
+        if jätk == 1:
+            print("algne 3, jätk 1")
--- projekt/mäng.py
+++ projekt/mäng.py
@@ -2,3 +2,5 @@ from funktsioon import *
 valik_ruum1_1 = algus()
 print("-"*97)
 valik_ruum1_2 = es_r_1(valik_ruum1_1)
+print("-"*97)
+valik_ruum1_3 = es_r_2(valik_ruum1_1, valik_ruum1_2)
